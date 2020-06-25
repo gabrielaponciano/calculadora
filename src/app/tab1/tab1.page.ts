c1442997098e7b5a3ce0c1035f4b7347f2cd3a43
gabrielaponciano <gabriela.ponciano@etec.sp.gov.br>
2020-06-24 21:42:43 -0300

    Projeto Calculadora

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -7,6 +7,42 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public valor1: number;
+  public valor2: number;
+  public resultado: number;
+  public adicao: number;
+  public multiplicacao: number;
+  public subtracao: number;
+  public divisao: number;
+  public limpa: null;
+
+  public somar(){
+    let adicao = this.valor1 + this.valor2;
+    this.adicao = adicao;
+  }
+  public multiplicar(){
+    let multiplicacao = this.valor1 * this.valor2;
+    this.multiplicacao = multiplicacao;
+  }
+  public subtrair(){
+    let subtracao = this.valor1 - this.valor2;
+    this.subtracao = subtracao;
+
+  }
+  public dividir(){
+    let divisao = this.valor1 / this.valor2;
+    this.divisao = divisao;
+
+  }
+  public limpar(){
+    let limpa = this.limpa;
+    this.valor1 = limpa
+    this.valor2 = limpa
+    this.adicao = limpa
+    this.multiplicacao = limpa
+    this.subtracao = limpa
+    this.divisao = limpa;
+  }
+
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { evaluate } from 'mathjs';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,99 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  // Variáveis globais
+  public calculo = ''; // Variável que exibe o cálculo na tela (vazia)
+  public resultado: string; // Variável que exibe o resultado na tela (null)
+
+  private ponto = false;
+
+  private operacoes = ['+', '-', '*', '/'];
+
+  constructor(public alertController: AlertController) {}
+
+  // Método para adicionar um número na tela
+  adicionarNumero(valor: string){ // valor -> número que o usuário digita tranformado em texto
+
+    if(this.resultado){
+      this.apagarTudo(); // Limpa a calculadora antes de inserir o próximo valor
+    }
+    this.calculo = this.calculo + valor;
+  }
+
+  // Método que verifica se tem algum ponto na tela para que o usuário não consiga colocar dois pontos ou mais.
+  adicionarPonto(){
+
+    if(this.ponto){
+      return; // return vazio -> executa um método parando a execução e retornando vazio
+    }
+  
+    // O código abaixo só é executado quando não estiver nenhum ponto inserido pelo usuário
+    this.calculo += "."; // Também pode ser escrita: this.calculo = this.calculo + valor;
+    this.ponto = true;
+  }
+
+  // Método que adiciona os cálculos seguidos um do outro
+  adicionarOperacao(operador: string){
+
+    // Usa o resultado do cálculo como continuação para a próxima operação
+    if(this.resultado){
+      this.calculo = this.resultado.toString();
+      this.resultado = null;
+    }
+
+    const ultimo = this.calculo.slice(-1);
+    if(this.operacoes.indexOf(ultimo) > -1){
+      return;
+    } 
+    // indexOf -> procura dentro da variável "operações" o último caractere
+
+    this.calculo += operador;
+    this.ponto = false; // Deixa o último ponto inserido como falso para que possa ser inserido outros pontos na mesma operação
+  }
+
+  // Método que zera totalmente a calculadora
+  public apagarTudo(){
+    this.calculo = ''; // Variável vazia
+    this.resultado = null; // Limpa tudo que está na variável resultado
+    this.ponto = false; // Limpa a calculadora permitindo inserir um ponto
+  }
+
+  // Método que zera apenas a última variável inserida pelo usuário
+  apagarUltimo(){
+    // Verifica se a última variável inserida é um ponto
+    const ultimo = this.calculo.slice(-1); // slice -> o método slice pega tudo que está dentro da variável calculo desde o início e desconsidera o último caractere
+    if(ultimo == '.'){
+      this.ponto = false;
+    }
+    this.calculo = this.calculo.slice(0, -1);
+  }
+
+  // Método que calcula o resultado das operações desejadas pelo usuário
+  public calcularResultado(){
+    // O "try" tenta executar o código para exibir o resultado e, caso de algum erro, o "catch" exibirá a mensagem de erro
+    try{
+      this.resultado = evaluate(this.calculo);
+    }catch (e) {
+      this.resultado = '';
+      this.presentAlert('ERRO', 'Cálculo inválido, verifique!');
+    }
+    
+  }
+  
+  // Método que exibe uma mensagem para o usuário
+  async presentAlert(titulo:string, mensagem:string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensagem,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
 }
+
+// COMENTÁRIOS ADICIONAIS
+// this -> faz referência as varriáveis globais criadas
+// Interpolação -> impressão de variáveis para colocar os textos na tela
+// Event Binding -> declaração de método (click) em um button para executar um evento
